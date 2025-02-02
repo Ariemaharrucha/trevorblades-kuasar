4c99dd51e735a880e9d308ced41d7474d489b1c2
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-02-02 22:58:10 +0700

    fetch country & edit card

--- src/features/home-page/HomePage.tsx
+++ src/features/home-page/HomePage.tsx
@@ -1,14 +1,54 @@
 import { Navbar } from "@/components/shared/Navbar"
 import { CardCountry } from "./components/CardCountry"
+import { GET_COUNTRIES } from "@/querry/querry"
+import { useQuery } from "@apollo/client";
+import useCountry from "@/store/useStore";
+import { useEffect } from "react";
+import { ICountry } from "@/types/country";
+import { Loader } from "lucide-react";
 
 export const HomePage = () => {
+  const { data, loading, error } = useQuery(GET_COUNTRIES);
+  const {  setCountries } = useCountry();
+
+  useEffect(() => {
+    if (data && data.countries) {
+      const formattedCountries = data.countries.map((country: ICountry) => ({
+        label: `${country.emoji} ${country.name}`,
+        value: country.name,
+      }));
+      setCountries(formattedCountries);
+    }
+  }, [data, setCountries]);
+
   return (
     <div>
       <Navbar></Navbar>
-      <div className="container mx-auto border-2 min-h-screen">
-        <div className="mt-2 mx-2  grid grid-cols-4 gap-4">
-          <CardCountry></CardCountry>
-        </div>
+      <div className="container mx-auto min-h-screen">
+      {loading || error ? (
+          <div className="flex items-center justify-center h-[50vh]">
+            <p className="text-lg  text-violet-700  p-4 rounded-lg shadow-md">
+              {error ? `❌ ${error.message}` : (
+                <div className="flex gap-2">
+                  <span className="animate-spin"><Loader></Loader></span>
+                  Loading...
+                </div>
+            )}
+            </p>
+          </div>
+        ) : (
+          <div className="mt-2 mx-2 grid grid-cols-4 gap-4">
+            {data?.countries.map((country: ICountry) => (
+              <CardCountry
+                key={country.code}
+                name={country.name}
+                emoji={country.emoji}
+                capital={country.capital}
+                currency={country.currency}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   )
--- src/features/home-page/components/CardCountry.tsx
+++ src/features/home-page/components/CardCountry.tsx
@@ -6,16 +6,26 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import React from "react"
 
-export const CardCountry = () => {
+type TCardProps = {
+  name: string
+  emoji: string,
+  capital?: string,
+  currency?: string,
+  code?: string
+}
+
+export const CardCountry:React.FC<TCardProps> = (props) => {
   return (
-    <Card className="col-span-1 border-violet-400 border-4">
+    <Card className="col-span-1 border-violet-400 border-4 flex flex-col overflow-hidden bg-purple-100/10">
       <CardHeader>
-        <CardTitle className="text-4xl">🇦🇺 Australia</CardTitle>
+        <CardTitle className="text-3xl text-center ">{props.name}</CardTitle>
+        <CardTitle className="text-3xl text-center">{props.emoji}</CardTitle>
       </CardHeader>
-      <CardContent className="space-y-2 text-lg">
-        <p><span className="font-semibold">Capital :</span> Canberra</p>
-        <p><span className="font-semibold">Currency :</span> AUD</p>
+      <CardContent className="space-y-2 text-lg flex-grow">
+        <p><span className="font-semibold">Capital :</span> {props.capital}</p>
+        <p><span className="font-semibold">Currency :</span> {props.currency}</p>
       </CardContent>
       <CardFooter className="">
         <Button size={'lg'} className="w-full border-none block   bg-violet-500 hover:bg-violet-600 text-white font-semibold text-lg ">Detail</Button>
