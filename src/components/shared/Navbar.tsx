6d83a061fd5c5e78dbec1d5f32b4060fc5de73a7
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-02-03 09:39:26 +0700

    edit navbar

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,40 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Menu } from "lucide-react";
+import { UserRound } from "lucide-react";
 
 type NavProps = {
-    to: string;
-    children: React.ReactNode;
-  };
+  to: string;
+  children: React.ReactNode;
+};
 
 export const Navbar = () => {
-  return (
-    <header className="bg-white sticky top-0 w-full z-10">
-        <nav className="flex justify-between items-center py-4 px-4 md:px-8 container mx-auto border-b-4 border-violet-600">
-          {/* Logo */}
-          <div>
-            <span className="font-bold text-2xl text-violet-700 cursor-pointer">Trevorblades</span>
-          </div>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-          {/* Desktop Navigation */}
-          <div className=" md:flex gap-8 items-center">
+  return (
+    <header className="bg-white sticky top-0 w-full z-10 border-b-4 border-violet-600">
+      <nav className="flex justify-between items-center py-4 px-4 md:px-8 container mx-auto">
+        {/* Logo */}
+        <div>
+          <span className="font-bold text-xl md:text-2xl text-violet-700 cursor-pointer">
+            Trevorblades
+          </span>
+        </div>
+
+        {/* Desktop Navigation */}
+        <div className=" hidden md:flex gap-8 items-center">
+          <Nav to="/">Home</Nav>
+          <Nav to="/chat-ai">Chat-AI</Nav>
+        </div>
+
+        {/* user profile */}
+        <div className="flex-1 md:flex-grow-0 justify-end flex gap-4 items-center text-violet-700 me-2">
+          <span className="text-lg font-semibold">Arie</span>
+          <UserRound size={40} className="border-2 rounded-full"/>
+        </div>
+
+        <div className="md:hidden">
+          <Menu size={30} className="cursor-pointer" onClick={()=> setIsMenuOpen(!isMenuOpen)}/>
+        </div>
+      </nav>
+
+      {/* Mobile Menu */}
+      {isMenuOpen && (
+          <div className="flex flex-col items-center bg-white py-4 space-y-3 md:hidden text-lg">
             <Nav to="/">Home</Nav>
             <Nav to="/chat-ai">Chat-AI</Nav>
           </div>
-
-          {/* user profile */}
-          {/* <div>
-            profile
-          </div> */}
-        </nav>
+        )}
     </header>
   );
 };
 
-const Nav:React.FC<NavProps> = ({to, children}) => {
+const Nav: React.FC<NavProps> = ({ to, children }) => {
   return (
     <NavLink
       to={to}
-    className={({isActive}) => isActive ? "text-lg font-semibold text-violet-700 transition-all" : "text-lg font-medium text-violet-600 hover:text-violet-700 hover:font-semibold transition-all"}
+      className={({ isActive }) =>
+        isActive
+          ? "text-lg font-semibold text-violet-700 transition-all"
+          : "text-lg font-medium text-violet-600 hover:text-violet-700 hover:font-semibold transition-all"
+      }
     >
       {children}
     </NavLink>
