435abe2eda192a4711d92e91f4aa14fef152f25e
Ariemaharrucha <arie.maharucha.zakka@gmail.com>
2025-02-04 10:07:40 +0700

    add style ui home page

--- src/features/home-page/HomePage.tsx
+++ src/features/home-page/HomePage.tsx
@@ -8,6 +8,7 @@ import { ICountry } from "@/types/country";
 import { Loader } from "lucide-react";
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
+import hero from '../../assets/map.png'
 
 export const HomePage = () => {
   const { data, loading, error } = useQuery(GET_COUNTRIES);
@@ -39,6 +40,24 @@ export const HomePage = () => {
     <div>
       <Navbar></Navbar>
       <div className="container mx-auto min-h-screen">
+      <div className="grid grid-cols-2 items-center min-h-screen">
+        <div className="md:col-span-1 col-span-2 px-8 space-y-3">
+        <h1 className="text-4xl font-bold text-violet-700">Discover the World, One Country at a Time!</h1>
+            <p className="text-lg text-gray-600">
+              Explore detailed information about every country.
+            </p>
+        </div>
+        <div className="md:col-span-1 col-span-2 ">
+        <div className="overflow-hidden mx-8 hidden md:block">
+            <img
+              src={hero}
+              alt=""
+              className="h-full w-full object-cover"
+              loading="lazy"
+            />
+          </div>
+        </div>
+      </div>
       {loading || error ? (
           <div className="flex items-center justify-center h-[50vh]">
             <div className="text-lg  text-violet-700  p-4 rounded-lg shadow-md">
@@ -52,8 +71,9 @@ export const HomePage = () => {
           </div>
         ) : (
           <div className="mt-2">
-
-            <div className="my-4 max-w-xl mx-auto">
+            <div className="mt-4 mb-6 max-w-xl mx-auto space-y-4 ">
+            <h2 className="text-2xl text-violet-700 font-semibold text-center">Find Your Country Instantly</h2>
+            <p className="text-center text-gray-600 mb-4">Select multiple countries and explore their details in seconds.</p>
             <Select
                 closeMenuOnSelect={false}
                 components={animatedComponents}
